372e91260006c9cf6f1258012486acad53246786
Arati Vaje <developerarati@gmail.com>
2023-01-19 23:47:04 +0530

    mongo client added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const uploadRouter = require("./src/routes/upload.route");
 const userRouteroute = require("./src/routes/user.route");
 const db = require("./src/config/db");
+db.connect();
 app.use(bodyParser.json());
 
 app.use("/",uploadRouter);
